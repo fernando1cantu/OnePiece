28e998492b41113540c3d233241fc92c7bd5579e
Fernando Cantu <ferjr.cantu@gmail.com>
2026-04-28 23:45:25 -0500

    more

--- syntax.hs
+++ syntax.hs
@@ -1,6 +1,6 @@
 module Syntax where
 
--- The One Piece 
+-- The One Piece
 
 {-
 
@@ -15,8 +15,8 @@ module Syntax where
         | if <condition> then [<stmt>] otherwise [<stmt>]
         | do <int> times [<stmt>]
 
-<condition> -> With <crew> 
-             | At <location> 
+<condition> -> With <crew>
+             | At <location>
              | LowHp
 
 <name> -> <string>
@@ -25,8 +25,8 @@ module Syntax where
 
 <player> -> <name> <hp> <location> (maybe <crew>) <totalcoins>
 
-<crew> -> Luffy | Zoro | Sanji | Nami | Usopp
-<villain> -> Kaido | Doflamingo | Blackbeard | Crocodile
+<crew> -> luffy | zoro | sanji | nami | usopp
+<villain> -> kaido | doflamingo | blackbeard | crocodile
 
 <location> -> island | marineford | wano
 
@@ -34,10 +34,21 @@ module Syntax where
 
 -}
 
--- Program 
-type Program = (Player, [Stmt])
+data Program = Program Player [Stmt]
 
--- Statements
+instance Show Program where
+    show (Program player stmts) =
+        "Program\n" ++
+        "=======\n" ++
+        "Starting Player:\n" ++
+        indent (show player) ++ "\n" ++
+        "Statements:\n" ++
+        indent (unlines (map show stmts))
+
+indent :: String -> String
+indent text =
+    unlines (map ("    " ++) (lines text))
+
 data Stmt
     = TeamUp Crew
     | Attack Villain
@@ -47,58 +58,129 @@ data Stmt
     | Print Player
     | If Condition [Stmt] [Stmt]
     | Do Int [Stmt]
-    deriving (Show)
 
--- Player
+instance Show Stmt where
+    show (TeamUp crew) =
+        "teamup " ++ show crew
+
+    show (Attack villain) =
+        "attack " ++ show villain
+
+    show (SailTo location) =
+        "sail to " ++ show location
+
+    show EatDevilFruit =
+        "eat devilfruit"
+
+    show LootTreasure =
+        "loot treasure"
+
+    show (Print player) =
+        "print player\n" ++ indent (show player)
+
+    show (If condition thenStmts elseStmts) =
+        "if " ++ show condition ++ " then\n" ++
+        indent (unlines (map show thenStmts)) ++
+        "otherwise\n" ++
+        indent (unlines (map show elseStmts))
+
+    show (Do times stmts) =
+        "do " ++ show times ++ " times\n" ++
+        indent (unlines (map show stmts))
+
 type Name = String
 type Hp = Int
 type TCoins = Int
 
 data Player = Player Name Hp Location (Maybe Crew) TCoins
-    -- deriving (Show)
-instance Show Player where
-    show (Player nm hp loc Nothing tc) = "Player " ++ nm ++ "\n"
-        ++ "Hp: " ++ show hp ++ "\n"
-        ++ "Location: " ++ show loc ++ "\n"
-        ++ "Crew: None\n"
-        ++ "Total Coins: " ++ show tc ++ "\n"
-    show (Player nm hp loc (Just crew) tc) =
-        "Player " ++ nm ++ "\n" ++
-        "Hp: " ++ show hp ++ "\n" ++
-        "Location: " ++ show loc ++ "\n" ++
-        "Crew: " ++ show crew ++ "\n" ++
-        "Total Coins: " ++ show tc ++ "\n"
 
--- Conditions 
+instance Show Player where
+    show (Player name hp location Nothing totalCoins) =
+        "Player: " ++ name ++ "\n" ++
+        "Hp: " ++ show hp ++ "\n" ++
+        "Location: " ++ show location ++ "\n" ++
+        "Crew: None\n" ++
+        "Total Coins: " ++ show totalCoins
+
+    show (Player name hp location (Just crew) totalCoins) =
+        "Player: " ++ name ++ "\n" ++
+        "Hp: " ++ show hp ++ "\n" ++
+        "Location: " ++ show location ++ "\n" ++
+        "Crew: " ++ show crew ++ "\n" ++
+        "Total Coins: " ++ show totalCoins
+
 data Condition
     = With Crew
     | At Location
     | LowHp
-    deriving (Show)
 
--- Crew 
+instance Show Condition where
+    show (With crew) =
+        "With " ++ show crew
+
+    show (At location) =
+        "At " ++ show location
+
+    show LowHp =
+        "LowHp"
+
 data Crew
     = Luffy
     | Zoro
     | Sanji
     | Nami
     | Usopp
-    deriving (Show)
+    deriving Eq
 
--- Villains 
+instance Show Crew where
+    show Luffy =
+        "luffy"
+
+    show Zoro =
+        "zoro"
+
+    show Sanji =
+        "sanji"
+
+    show Nami =
+        "nami"
+
+    show Usopp =
+        "usopp"
+
 data Villain
     = Kaido
     | Doflamingo
     | Blackbeard
     | Crocodile
-    deriving (Show)
 
--- Locations
+instance Show Villain where
+    show Kaido =
+        "kaido"
+
+    show Doflamingo =
+        "doflamingo"
+
+    show Blackbeard =
+        "blackbeard"
+
+    show Crocodile =
+        "crocodile"
+
 data Location
     = Island
     | Marineford
     | Wano
-    deriving (Show)
+    deriving Eq
 
+instance Show Location where
+    show Island =
+        "island"
+
+    show Marineford =
+        "marineford"
+
+    show Wano =
+        "wano"
 
 type Env = Player
